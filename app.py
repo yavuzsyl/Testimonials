83f0d60be280bc26d017d47edefd02cd5c0a50a8
yavuzsyl <yavuzsyl@gmail.com>
2022-10-08 03:36:19 +0300

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'hello'
-
+ 
 @app.route('/api/testimonials')
 def testimonials():
-    return {'testimonails':['great','its ok', 'fantastic']}
+    return {'testimonails':['great','its ok', 'fantastic','noice']}
