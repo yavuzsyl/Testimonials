2cc9474c19d55d0e40e7fb9e9b836e4a1bd8808a
yavuzsyl <yavuzsyl@gmail.com>
2022-10-10 08:55:48 +0300

    db con str

--- testimonials/__init__.py
+++ testimonials/__init__.py
@@ -4,13 +4,13 @@ import os
 
 app = Flask(__name__)
 
-# DB_NAME = os.environ.get('RDS_DB_NAME')
-# DB_USERNAME = os.environ.get('RDS_USERNAME')
-# DB_PASSWORD = os.environ.get('RDS_PASSWORD')
-# DB_HOSTNAME = os.environ.get('RDS_HOSTNAME')
+DB_NAME = os.environ.get('RDS_DB_NAME')
+DB_USERNAME = os.environ.get('RDS_USERNAME')
+DB_PASSWORD = os.environ.get('RDS_PASSWORD')
+DB_HOSTNAME = os.environ.get('RDS_HOSTNAME')
 
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'postgres://testimonials:q9OkCVqzYloxnsOPakjC@testimonials.c9z8hhscom0a.us-east-1.rds.amazonaws.com/postgres'
+    'SQLALCHEMY_DATABASE_URI'] = f'postgres://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
